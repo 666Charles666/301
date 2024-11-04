e157302b04515b4e96a7be1598446060530dc86d
666Charles666 ZRC <147719423+666Charles666@users.noreply.github.com>
2024-11-04 21:21:19 +0800

    Update Case.java

--- _301_jewelry store/src/Case.java
+++ _301_jewelry store/src/Case.java
@@ -1,10 +1,33 @@
+import java.util.LinkedList;
 import java.util.List;
 
 public class Case {
-    //the only identifier
-    private static int caseNum;
-
+    private static int caseNum,size;
     private String type , lighting;
+    private List<Tray>[] trays;
+    public Case(int size){
+        trays = new List[size];
+        for (int i=0;i < size; i++){
+            trays[i] = new LinkedList<>();
+        }
+    }
+    public int hushFunction(int key){
+        return Math.abs(key%trays.length);
+    }
 
-    List<Tray> trays;
+    public int add(Tray item){
+        int key = item.hashCode();
+        int index = hushFunction(key);
+        trays[index].add(item);
+        return index;
+    }
+    public void displayHashTable(){
+        for (int i = 0; i < trays.length; i++) {
+            System.out.print("Slot " + i + ": ");
+            for (Tray item : trays[i]) {
+                System.out.print(item + " -> ");
+            }
+            System.out.println("null");
+        }
+    }
 }
