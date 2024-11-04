edc240eb6b4c1c0a8e1fb62d40f6cd2b72efeb55
666Charles666 ZRC <147719423+666Charles666@users.noreply.github.com>
2024-11-04 21:20:51 +0800

    Update JewelryShop.java

--- _301_jewelry store/src/JewelryShop.java
+++ _301_jewelry store/src/JewelryShop.java
@@ -1,8 +1,34 @@
+import java.util.LinkedList;
 import java.util.List;
 
 /**
  * hash->hash->link
  */
 public class JewelryShop {
-    List<Case> cases;
+    private List<Case>[] cases;
+    public JewelryShop(int size){
+        cases =new List[size];
+        for (int i = 0; i < size; i++) {
+            cases[i] = new LinkedList<>();
+        }
+    }
+    public int hushFunction(int key){
+        return Math.abs(key%cases.length);
+    }
+
+    public int add(Case item){
+        int key = item.hashCode();
+        int index = hushFunction(key);
+        cases[index].add(item);
+        return index;
+    }
+    public void displayHashTable(){
+        for (int i = 0; i < cases.length; i++) {
+            System.out.print("Slot " + i + ": ");
+            for (Case item : cases[i]) {
+                System.out.print(item + " -> ");
+            }
+            System.out.println("null");
+        }
+    }
 }
