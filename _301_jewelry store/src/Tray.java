72842313de15e0ae00f1707ad9ccf9b99432b333
666Charles666 ZRC <147719423+666Charles666@users.noreply.github.com>
2024-11-08 20:09:48 +0800

    Update Tray.java
    Added a specification method for ids

--- _301_jewelry store/src/Tray.java
+++ _301_jewelry store/src/Tray.java
@@ -3,15 +3,16 @@ import java.util.List;
 public class Tray {
      private String TrayID;
      private String inlay , material ,color;
-     private double dimension;
+     private double length,width;
      private int size;
 
-     public Tray (Tray trayInfo,Case caseInstance){
-          this.size = trayInfo.getSize();
-          this.inlay = trayInfo.getInlay();
-          this.material = trayInfo.getMaterial();
-          this.color = trayInfo.getColor();
-          this.dimension = trayInfo.getDimension();
+     public Tray (int size,String inlay,String material,String color,double length,double width,Case caseInstance){
+          this.size = size;
+          this.inlay = inlay;
+          this.color = color;
+          this. material = material;
+          this. width = width;
+          this.length = length;
           TrayID = generateTrayID(caseInstance);
      }
      class Node {
@@ -41,10 +42,10 @@ public class Tray {
                System.out.println("ADD SUCCESSFUL");
           }
           else {
-              newNode.prev = tail;
-              tail.next = newNode;
-              tail = newNode;
-              System.out.println("ADD SUCCESSFUL");
+               newNode.prev = tail;
+               tail.next = newNode;
+               tail = newNode;
+               System.out.println("ADD SUCCESSFUL");
           }
           size ++;
      }
@@ -60,10 +61,18 @@ public class Tray {
           }
           return null;
      }
+     private String calculate(int num){
+          if (num >= 1 && num <= 9) {
+               // 如果数字在1到9之间，返回格式化为两位数的字符串
+               return String.format("%02d", num);  // 格式化为两位数，如 01, 02, ..., 09
+          }
+          // 如果数字大于9，直接返回数字的字符串
+          return String.valueOf(num);  // 或者 return Integer.toString(num);
+     }
      private String generateTrayID(Case caseInstance){
           if (caseInstance.getTrayIdentifier() < 'Z'){
                char current = caseInstance.getTrayIdentifier();
-               return (String.valueOf(current) + caseInstance.getNextTrayNum());
+               return (String.valueOf(current) + calculate(caseInstance.getNextTrayNum()));
           }
           else return "E";
      }
@@ -93,13 +102,22 @@ public class Tray {
           this.color = color;
      }
 
-     public double getDimension() {
-          return dimension;
+     public double getLength() {
+          return length;
      }
 
-     public void setDimension(double dimension) {
-          this.dimension = dimension;
+     public void setLength(double length) {
+          this.length = length;
      }
+
+     public double getWidth() {
+          return width;
+     }
+
+     public void setWidth(double width) {
+          this.width = width;
+     }
+
      public String getTrayID(){
           return this.TrayID;
      }
